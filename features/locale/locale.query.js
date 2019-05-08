83df0add47212610c8da419a62eb3f6bd1af2df7
Marco Pegoraro <marco.pegoraro@gmail.com>
2019-05-08 08:29:27 +0200

    Remove services and features to the react-ssr

--- features/locale/locale.query.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.default = "\nquery locale (\n    $locale: String!\n) {\n    locale (locale: $locale) {\n        locale\n        messages\n    }\n}";
